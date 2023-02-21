9518959924390e0fffbf0a768240e5d551ddedce
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-02-21 23:42:34 +0100

    added indication of number of photos in a report

--- Client/src/Components/Reports.js
+++ Client/src/Components/Reports.js
@@ -11,15 +11,17 @@ const Reports = (props) => {
     <ul>
       {props.reports.slice(0).reverse().map(report => (
         <li className={classes.reportItem} key={report.dateAndTime}>
-          <p>{report.dateAndTime}<br /><small>{report.institution} <br /> {report.causeDescription} <br />Lokacija: {report.location}</small> </p>      
+          <p className={classes.liHeader}>{report.dateAndTime}<br /><small>{report.institution} <br /> {report.causeDescription} <br />Lokacija: {report.location}</small> </p>      
           <p><b>{report.title}</b></p>
           <p>{report.description}</p>
-          <div className={classes.icons}>
-
-            <span className={classes.penIcon}>
-              <EditReportButton email={props.email} report={report} onUpdatedReport={props.onDel} />
+          <div className={classes.liFooter}>          
+            <p className={classes.alignLeft}><small>Fotografije: </small><b>{report.pic1 ? '☑' : '☐'}{report.pic2 ? '☑' : '☐'}{report.pic3 ? '☑' : '☐'}</b></p>
+            <span className={classes.alignRight}>
+              <span className={classes.penIcon}>
+                <EditReportButton email={props.email} report={report} onUpdatedReport={props.onDel} />
+              </span>  
+              <DeleteReportButton onDel={props.onDel} report={report} />
             </span>
-            <DeleteReportButton onDel={props.onDel} report={report} />
           </div>
         </li>
       ))}
