02bf8ad5d00cb1a9a3ef68e2edcb1f5773e9e78b
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-02-26 23:44:38 +0100

    added-fetch-context for storing url data

---
+++ Client/src/Store/fetch-context.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FetchContext = React.createContext(
+    {
+        protocol: "https://",
+        //host: "localhost:",
+        host:"brankobjelic.duckdns.org:",
+        port: "7281/"
+    }
+)
+
+export default FetchContext
