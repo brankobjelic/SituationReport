df803550eca1be8330911796e7120701d0219093
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-03-12 14:33:04 +0100

    Changed website title

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -4,13 +4,13 @@ import classes from './Header.module.css';
 const Header = (props) => {
   return (
     <header className={classes.header}>
-      <h1>Situation Report app</h1>
+      <h1>Prijavi problem</h1>
         <div id="signInDiv"></div>
         {Object.keys(props.user).length !== 0 &&
             <>
         
             <div>
-              <button className={classes.button} onClick={ (e) => props.handleSignOut(e)}>Sign Out</button>
+              <button className={classes.button} onClick={ (e) => props.handleSignOut(e)}>Odjavi se</button>
                 <img className={classes.userImage} src={props.user.picture} alt="" referrerPolicy="no-referrer"></img>
                 <p className={classes.username}>{props.user.name}</p>                
             </div>
