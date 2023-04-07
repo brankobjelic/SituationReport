cdeafccf990c42d1cd8ca0b16f14a35dc36b11eb
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-04-08 00:25:44 +0200

    added some server form validation

--- SituationReport/Models/ReportDTO.cs
+++ SituationReport/Models/ReportDTO.cs
@@ -5,12 +5,14 @@ namespace SituationReport.Models
     public class ReportDTO
     {
         [Required]
+        [EmailAddress]
         public string? userEmail { get; set; }
         [Required]
         public int causeId { get; set; }
         public string? Location { get; set; }
         public Decimal? Latitude { get; set; }
         public Decimal? Longitude { get; set; }
+        [StringLength(200)]
         public string? Title { get; set; }
         [Required]
         public string? Description { get; set; }
