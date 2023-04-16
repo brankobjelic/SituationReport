8f6fd9d307f296b96dee2282d2c14a54972f6a9b
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-04-16 22:12:51 +0200

    added FrequentCauses endpoint

--- SituationReport/Repository/CauseRepository.cs
+++ SituationReport/Repository/CauseRepository.cs
@@ -112,5 +112,44 @@ namespace SituationReport.Repository
 
             return i;
         }
+
+        public List<Cause> GetFrequent()
+        {
+            string query = "select top 3 causeid, count(*), causes.Description from reports left join Causes " +
+                "on causeid=causes.Id group by causeid, Causes.Description order by count(*) desc";
+
+            string connectionString = Configuration.GetConnectionString("AppConnectionString");
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = connection.CreateCommand();
+
+            connection.Open();
+
+            command.CommandText = query;
+
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+
+            adapter.Fill(ds, "Causes");
+            dt = ds.Tables["Causes"];
+
+            command.Dispose();
+            connection.Close();
+
+
+            List<Cause> causes = new List<Cause>();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Cause c = new Cause();
+                c.Id = (int)dr["causeid"];
+                c.Description = dr["Description"].ToString();
+                causes.Add(c);
+            }
+            return causes;
+        }
     }
 }
