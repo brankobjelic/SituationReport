f956653dc17333394d3d0dce1aff5a01f5c04a24
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-04-25 23:37:04 +0200

    added Google Id token verifying service

---
+++ SituationReport/Services/OAuthService.cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Auth;
+
+namespace SituationReport.Services
+{
+    public static class OAuthService
+    {
+        public static async Task<GoogleJsonWebSignature.Payload?> VerifyGoogleTokenId(string token)
+        {
+            try
+            {
+                // uncomment these lines if you want to add settings: 
+                // var validationSettings = new GoogleJsonWebSignature.ValidationSettings
+                // { 
+                //     Audience = new string[] { "yourServerClientIdFromGoogleConsole.apps.googleusercontent.com" }
+                // };
+                // Add your settings and then get the payload
+                // GoogleJsonWebSignature.Payload payload =  await GoogleJsonWebSignature.ValidateAsync(token, validationSettings);
+
+                // Or Get the payload without settings.
+                GoogleJsonWebSignature.Payload payload = await GoogleJsonWebSignature.ValidateAsync(token);
+
+                return payload;
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
