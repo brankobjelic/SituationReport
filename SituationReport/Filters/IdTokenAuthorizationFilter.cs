03968a4c74620e1d197c3bfbf095cfacbc1397c8
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-04-26 23:43:14 +0200

    created custom authorization filter

---
+++ SituationReport/Filters/IdTokenAuthorizationFilter.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SituationReport.Interfaces;
+
+namespace SituationReport.Filters
+{
+
+    public class CustomAuthorizeAttribute : ServiceFilterAttribute
+    {
+        public CustomAuthorizeAttribute()
+            : base(typeof(CustomAuthorizeFilter))
+        {
+
+        }
+    }
+    public class CustomAuthorizeFilter : Attribute, IAuthorizationFilter
+    {
+        private readonly IUserRepository userRepository;
+        public CustomAuthorizeFilter(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var result = true;
+            if(!context.HttpContext.Request.Headers.ContainsKey("Authorization"))
+                result = false;
+
+            string token = string.Empty;
+            string email = string.Empty;
+            if (result)
+            {
+                token = context.HttpContext.Request.Headers.First(x => x.Key == "Authorization").Value;
+                email = context.HttpContext.Request.Headers.First(x => x.Key == "Email").Value;
+                if(userRepository.GetUserToken(email) != token)
+                {
+                    result = false;
+                }
+                if (!result)
+                {
+                    context.ModelState.AddModelError("Unauthorized", "You are not authorized.");
+                    context.Result = new UnauthorizedObjectResult(context.ModelState);
+                }
+            }
+        }
+    }
+}
