4828f51e316b41704d871b229f90432566f005ca
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-05-27 11:39:46 +0200

    no message

--- SituationReport/Controllers/UsersController.cs
+++ SituationReport/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SituationReport.Filters;
 using SituationReport.Interfaces;
