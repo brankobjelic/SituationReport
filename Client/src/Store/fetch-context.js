acb4635b7855a3d61e1a9b1046d3471026a3ce2b
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-05-27 11:31:26 +0200

    no message

--- Client/src/Store/fetch-context.js
+++ Client/src/Store/fetch-context.js
@@ -5,6 +5,7 @@ const FetchContext = React.createContext(
         protocol: "https://",
         //host: "localhost:",
         host:"brankobjelic.duckdns.org:",
+        //host:"ec2-3-82-155-229.compute-1.amazonaws.com:",
         port: "7281/"
     }
 )
