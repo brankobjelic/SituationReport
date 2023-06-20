faa659569c29c898f0cf0cf51afda0e39686fb18
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-06-20 21:06:39 +0200

    added username field to User object model

--- SituationReport/Models/User.cs
+++ SituationReport/Models/User.cs
@@ -6,6 +6,7 @@ namespace SituationReport.Models
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public string? Username { get; set; }
         [EmailAddress]
         [Required(ErrorMessage = "Email is required")]
         public string? Email { get; set; }
