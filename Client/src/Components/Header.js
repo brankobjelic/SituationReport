a7d9aeaf92df2902e12e44291f00f7bd1ce83afa
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-06-29 22:07:34 +0200

    added functionality for showing and hiding modals when selected from navigation menu

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -2,10 +2,30 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import classes from './Header.module.css';
 import ContactForm from './ContactForm';
+import Navigation from './Navigation';
+import UsernameForm from './UsernameForm';
+import About from './About';
 
 const Header = (props) => {
 
   const [showContactForm, setShowContactForm] = useState(false)
+  const [showUsernameForm, setShowUsernameForm] = useState(false)
+  const [showAboutModal, setShowAboutModal] = useState(false)
+
+  useEffect(() => {
+    if (showContactForm || showUsernameForm || showAboutModal) {
+      document.body.style.overflow = "hidden";
+      document.body.addEventListener('touchmove', preventIOSScroll);
+    }else{
+      document.body.style.overflowX = "auto";
+      document.body.style.overflowY = "scroll";
+    }
+  },[showContactForm, showUsernameForm, showAboutModal])
+
+  //prevents scrolling of background behind modal on IOS devices
+  const preventIOSScroll = (e) => {
+    e.preventDefault();
+  }
 
   function showContactFormHandler(){
     setShowContactForm(true)
@@ -15,19 +35,26 @@ const Header = (props) => {
     setShowContactForm(false)
   }
 
-  //prevents scrolling of background behind modal on IOS devices
-  const preventIOSScroll = (e) => {
-    e.preventDefault();
+  function showUsernameFormHandler(){
+    setShowUsernameForm(true)
   }
 
-  useEffect(() => {
-    if (showContactForm){
-      document.body.style.overflow = "hidden";
-      document.body.addEventListener('touchmove', preventIOSScroll);
-    }else{
-      document.body.style.overflow = "scroll";
-    }
-  },[showContactForm])
+  function updateUsernameFormHandler(name){
+    props.onUpdatedUsername(name)
+    setShowUsernameForm(false)
+  }
+
+  function leaveUsernameFormHandler(){
+    setShowUsernameForm(false)
+  }
+
+  function showAboutModalHandler(){
+    setShowAboutModal(true)
+  }
+
+  function leaveAboutModalHandler(){
+    setShowAboutModal(false)
+  }
 
   return (
     <header className={classes.header}>
@@ -42,8 +69,10 @@ const Header = (props) => {
               </div>
             </>
         }
-        <b className={classes.menuItem} onClick={showContactFormHandler}>Kontaktirajte nas</b>
+        <Navigation user={props.user} onShowUsernameForm={showUsernameFormHandler} onShowContactForm={showContactFormHandler} onShowAboutModal={showAboutModalHandler} />
         {showContactForm && <ContactForm onLeaveContactForm={leaveContactFormHandler} />}
+        {showUsernameForm && <UsernameForm  email={props.user.email} name={props.username} onUpdatedUsername={updateUsernameFormHandler} onLeaveUsernameForm={leaveUsernameFormHandler} nameChange={true} />}
+        {showAboutModal && <About onLeaveAboutModal={leaveAboutModalHandler} />}
     </header>
   )
 }
