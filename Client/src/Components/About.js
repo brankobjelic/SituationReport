738be2e98f9b2f868d57797b16fa17dff2c7eae3
brankobjelic <33072204+brankobjelic@users.noreply.github.com>
2023-07-09 12:14:46 +0200

    Updated About text

--- Client/src/Components/About.js
+++ Client/src/Components/About.js
@@ -10,10 +10,10 @@ const About = (props) => {
       <form className={`${classes['modal-content']} ${classes['form-style-1']}`}>
         <FontAwesomeIcon onClick={props.onLeaveAboutModal} icon={faCircleXmark} className={classes.boxclose} size = '2x'/>
         <br />
-        <p>Moj komunalni  pomoćnik je platforma koja omogućava da na jednostavan način prijavite probleme sa kojima se susrećete svakodnevno, a možda ih do sada niste prijavili jer niste znali kome da se obratite.</p>
+        <p>Moj komunalni  pomoćnik je platforma koja omogućava da na jednostavan način prijavite komunalne probleme sa kojima se susrećete svakodnevno, a možda ih do sada niste prijavili jer niste znali kome da se obratite.</p>
         <p>Aplikacija Moj komunalni pomoćnik Vas oslobađa nedoumica ili pozivanja neodgovarajućih institucija, koje Vas onda prusmeravaju na druge, treće... Zvuči poznato?</p>
-        <p>Umesto ovog mukotrpnog procesa, zbog kojeg često oni manje strpljivi i odustaju od prijavljivanja, dovoljno je da nakon logovanja na aplikaciju uz pomoć svog Google naloga, otvorite novu prijavu, izaberete jednu od ponuđenih vrsta problema, i vaša prijava će biti poslata na pravu adresu. Ukoliko želite, možete odmah i pozvati nadležnu instituciju preko linka u formi.
-        </p>
+        <p>Umesto ovog mukotrpnog procesa, zbog kojeg često oni manje strpljivi i odustaju od prijavljivanja, dovoljno je da nakon logovanja na aplikaciju uz pomoć svog Google naloga, otvorite novu prijavu, izaberete jednu od ponuđenih vrsta problema, i vaša prijava će biti poslata na pravu adresu. Ukoliko želite, možete odmah i pozvati nadležnu instituciju preko linka u formi.</p>
+        <p>Aplikacija trenutno omogućava prijavljivanje za teritoriju grada Novog Sada.</p>
       </form>
     </div>
   );
